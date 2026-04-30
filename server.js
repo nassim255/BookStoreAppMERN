91f473bd4c91c87157a72107e227d15281eddcfd
Kherfouchi Nedjemeddine Nassim <nassim.kherfouchi@gmail.com>
2026-04-30 16:30:00 +0100

    fix API URL for deployment

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-const cors = require("cors")
-const dotenv = require("dotenv").config()
-const cookieParser = require("cookie-parser")
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+const cookieParser = require("cookie-parser");
 
-const connectDB = require("./config/db")
+const connectDB = require("./config/db");
 app.use(cookieParser());
 
 connectDB();
@@ -14,17 +14,20 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
+app.use("/users", require("./routes/users"));
+app.use("/books", require("./routes/books"));
+app.use("/category", require("./routes/category"));
+app.use("/admin", require("./routes/admin"));
+app.use("/carts", require("./routes/carts"));
 
-app.use("/users" ,require("./routes/users"))
-app.use("/books" ,require("./routes/books"))
-app.use("/category" ,require("./routes/category"))
-app.use("/admin" ,require("./routes/admin"))
-app.use("/carts" ,require("./routes/carts"))
+app.use("/images", express.static("images"));
 
-app.use("/images",express.static("images"))
+const PORT = process.env.PORT || 3000;
 
-const PORT  = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
 
-app.listen(PORT,()=> {
-    console.log(`server is running on port ${PORT}`)
-})
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
